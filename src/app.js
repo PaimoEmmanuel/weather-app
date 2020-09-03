50c5b1344455befbfe97ed8d13f034b3f875c475
Paimo Emmanuel <paimoemmanuel2016@gmail.com>
2020-09-03 19:05:10 +0100

    setup app for Heroku

--- src/app.js
+++ src/app.js
@@ -3,8 +3,9 @@ const express = require('express');
 const hbs = require('hbs');
 const geocode = require("./utils/geocode");
 const forecast = require('./utils/forecast');
-const app = express();
 
+const app = express();
+const port = process.env.PORT || 3000;
 //Define path for express config
 const publicPath = path.join(__dirname, "../public");
 const viewsPath = path.join(__dirname, '../templates/views')
@@ -86,6 +87,6 @@ app.get('*', (req, res) => {
         message: 'Page article not found'
     })
 })
-app.listen(3000, () => {
-    console.log('Express is running on port 3000')
+app.listen(port, () => {
+    console.log('Express is running on port ' + port);
 });
